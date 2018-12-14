836616e2f2fdb672c9053294d2c6fe03b434f7f4
CryChow <yuezhou@sf-express.com>
2018-12-14 19:14:03 +0800

    :art: 统一代码风格

--- routes/essay.js
+++ routes/essay.js
@@ -2,7 +2,7 @@ const Router = require('koa-router')
 const essay = new Router()
 
 essay.get('/', async function (ctx, next) {
-  ctx.body = 'this a users response!';
+  ctx.body = 'this a users response!'
 })
 
 module.exports = essay
