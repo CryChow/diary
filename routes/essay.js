2fa30516de5db5478afd755a2c680bfedb0b383e
CryChow <yuezhou@sf-express.com>
2018-12-14 19:40:53 +0800

    :sparkles: 添加essay新增接口

--- routes/essay.js
+++ routes/essay.js
@@ -1,8 +1,28 @@
 const Router = require('koa-router')
-const essay = new Router()
+const Essay = require('../model/essay')
 
-essay.get('/', async function (ctx, next) {
+const essayRouter = new Router()
+
+essayRouter.get('/add', async function (ctx) {
+  const {
+    title,
+    body,
+    author,
+    tags,
+    password,
+  } = ctx.query
+  const essay = new Essay({
+    title,
+    body,
+    author,
+    tags,
+    password,
+  })
+  essay.insert()
+    .then(res => {
+      console.log(res, 'success')
+    })
   ctx.body = 'this a users response!'
 })
 
-module.exports = essay
+module.exports = essayRouter
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,12 @@
 const Router = require('koa-router')
 const router = new Router()
 
-const Essay = require('../model/essay')
 const essay = require('./essay')
 
-
-router.get('/welcome', async function (ctx, next) {
-  const essay = new Essay({
-    title: 'test title',
-    body: 'test body',
-    author: 'CryChow',
-    tags: 'test,one,two',
-  })
-  essay.insert()
-    .then(res => {
-      console.log(res)
-    })
+router.get('/welcome', async function (ctx) {
   ctx.body = 'this a users response!'
 })
+
 router.use('/essay', essay.routes())
+
 module.exports = router
